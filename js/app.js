109c569e376a5132cbe37edf7fa2780a104a8d28
horler408 <horler.web@gmail.com>
2020-10-14 10:24:02 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,165 @@
+/*Parameters
+description — A search term, such as "ruby" or "java". This parameter is aliased to search.
+location — A city name, zip code, or other location search term.
+lat — A specific latitude. If used, you must also send long and must not send location.
+long — A specific longitude. If used, you must also send lat and must not send location.
+full_time — If you want to limit results to full time positions set this parameter to 'true'.
+page=1
+*/
+
+class jobs = {
+  // <!-- Item Start -->
+  // Manage 👍
+  // New!
+  // Featured
+  // Fullstack Developer
+  // 1d ago
+  // Part Time
+  // Remote
+  // <!-- Role -->
+  // Fullstack
+  // <!-- Level -->
+  // Midweight
+  // <!-- Languages -->
+  // Python
+  // <!-- Tools -->
+  // React
+  // <!-- Item End -->
+
+  // <!-- Item Start -->
+  // Account
+  // New!
+  // Junior Frontend Developer
+  // 2d ago
+  // Part Time
+  // USA only
+  // <!-- Role -->
+  // Frontend
+  // <!-- Level -->
+  // Junior
+  // <!-- Languages -->
+  // JavaScript
+  // <!-- Tools -->
+  // React
+  // Sass
+  // <!-- Item End -->
+
+  // <!-- Item Start -->
+  // MyHome
+  // Junior Frontend Developer
+  // 5d ago
+  // Contract
+  // USA only
+  // <!-- Role -->
+  // Frontend
+  // <!-- Level -->
+  // Junior
+  // <!-- Languages -->
+  // CSS
+  // JavaScript
+  // <!-- Item End -->
+
+  // <!-- Item Start -->
+  // Loop Studios
+  // Software Engineer
+  // 1w ago
+  // Full Time
+  // Worldwide
+  // <!-- Role -->
+  // Fullstack
+  // <!-- Level -->
+  // Midweight
+  // <!-- Languages -->
+  // JavaScript
+  // Ruby
+  // <!-- Tools -->
+  // Sass
+  // <!-- Item End -->
+
+  // <!-- Item Start -->
+  // FaceIt
+  // Junior Backend Developer
+  // 2w ago
+  // Full Time
+  // UK only
+  // <!-- Role -->
+  // Backend
+  // <!-- Level -->
+  // Junior
+  // <!-- Languages -->
+  // Ruby
+  // <!-- Tools -->
+  // RoR
+  // <!-- Item End -->
+
+  // <!-- Item Start -->
+  // Shortly
+  // Junior Developer
+  // 2w ago
+  // Full Time
+  // Worldwide
+  // <!-- Role -->
+  // Frontend
+  // <!-- Level -->
+  // Junior
+  // <!-- Languages -->
+  // HTML
+  // JavaScript
+  // <!-- Tools -->
+  // Sass
+  // <!-- Item End -->
+
+  // <!-- Item Start -->
+  // Insure
+  // Junior Frontend Developer
+  // 2w ago
+  // Full Time
+  // USA only
+  // <!-- Role -->
+  // Frontend
+  // <!-- Level -->
+  // Junior
+  // <!-- Languages -->
+  // JavaScript
+  // <!-- Tools -->
+  // Vue
+  // Sass
+  // <!-- Item End -->
+
+  // <!-- Item Start -->
+  // Eyecam Co.
+  // Full Stack Engineer
+  // 3w ago
+  // Full Time
+  // Worldwide
+  // <!-- Role -->
+  // Fullstack
+  // <!-- Level -->
+  // Midweight
+  // <!-- Languages -->
+  // JavaScript
+  // Python
+  // <!-- Tools -->
+  // Django
+  
+
+  // <!-- Item Start -->
+  // The Air Filter Company
+  // Front-end Dev
+  // 1mo ago
+  // Part Time
+  // Worldwide
+  // // Role
+  // Frontend
+  // //Level
+  // Junior
+  // //Languages
+  // JavaScript
+  // //Tools
+  // React
+  // Sass
+}
+//Accessing DOM
+const count = document.querySelector('.count')
+
+
